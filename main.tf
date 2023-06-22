058b77f2e46315e143d0a23060770723d4b2bdc1
Dan Rollo <danrollo@gmail.com>
2023-06-22 15:47:34 -0400

    From @hanstdam: Use an SSL Policy that does not support the deprecated TLS 1.0 and 1.1 protocols.

--- main.tf
+++ main.tf
@@ -305,7 +305,7 @@ resource "aws_lb_target_group" "target_group" {
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = "${aws_alb.application_load_balancer.arn}"
   certificate_arn   = aws_acm_certificate.this.arn
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  ssl_policy        = "ELBSecurityPolicy-FS-1-2-Res-2020-10"
   port              = "443"
   protocol          = "HTTPS"
 
